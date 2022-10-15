a52db08b4a094b1429a9e5cc383012a5229813d9
Thiago Silveira <thiago1992@yahoo.com>
2022-10-15 16:33:22 -0600

    Feat: Catalog service returns nearby breweries
    Co-authored-by: Bryce Simonds <bsimonds14@gmail.com>
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryan Shears <brshears@gmail.com>

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,12 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
+
+  validates_presence_of :catalog_beer_id
+  validates_presence_of :name
+  validates_presence_of :style
+  validates_presence_of :description
+  validates_presence_of :abv
+  validates_presence_of :ibu
+  validates_presence_of :catalog_brewery_id
+  validates_presence_of :last_modified
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,2 +1,13 @@
 class Brewery < ApplicationRecord
+  has_many :beers
+
+  validates_presence_of :catalog_brewery_id
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :distance_from_user
+  validates_presence_of :website
+  validates_presence_of :instagram
+  validates_presence_of :facebook
+  validates_presence_of :twitter
+  validates_presence_of :brewery_description
 end

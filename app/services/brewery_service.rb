40c660f0202f4b1dc474efa11ca9aaba22a3a0d1
Bryan Shears <brshears@gmail.com>
2022-10-16 13:45:13 -0600

    update config.yml

---
+++ app/services/brewery_service.rb
@@ -0,0 +1,20 @@
+class BreweryService 
+  class << self
+    def get_url(url, location)
+      conn = Faraday.new(url: 'https://api.catalog.beer/location/nearby') do |faraday|
+        faraday.params[:key] = ENV['catalog_api_key']
+        faraday.params[:location] = location
+      end
+
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def get_breweries(location)
+      get_url("/geocoding/v1/address", location) 
+    end
+
+  end
+
+
+end 

a1ce72cf1e4dab18d01fec2aae58cfacb3c525c9
Parker Thomson <pdthomson1239@gmail.com>
2022-10-16 15:09:27 -0400

    create rake tasks to populate brewery data and delete the DataBase and reset the keys of the database quote> quote> quote> Co-authored-by: Bryce Simonds <bsimonds14@gmail.com> quote> Co-authored-by: Bryan Shears <brshears@gmail.com> quote> Co-authored-by: Thiago Silveira  <thiago1992@yahoo.com>

--- app/facades/catalog_facade.rb
+++ app/facades/catalog_facade.rb
@@ -8,7 +8,7 @@ class CatalogFacade
       end
 
       brewery_objects.map do |brewery_object|
-        Brewery.create!(address: brewery_object.address, name: brewery_object.name, catalog_brewery_id: brewery_object.catalog_brewery_id, distance_from_user: brewery_object.distance_from_user, website: brewery_object.website, facebook: brewery_object.facebook, instagram: brewery_object.instagram, twitter: brewery_object.twitter, brewery_description: brewery_object.brewery_description)
+        Brewery.find_or_create_by(address: brewery_object.address, name: brewery_object.name, catalog_brewery_id: brewery_object.catalog_brewery_id, distance_from_user: brewery_object.distance_from_user, website: brewery_object.website, facebook: brewery_object.facebook, instagram: brewery_object.instagram, twitter: brewery_object.twitter, brewery_description: brewery_object.brewery_description)
       end
     end
   end

858e98cd81f6efa7ee3dd996414c6d72b32f36f6
Thiago Silveira <thiago1992@yahoo.com>
2022-10-18 22:59:46 -0600

    Test: Brewer beers facade returns beers for each brewery
    Co-authored-by: Bryan Shears <brshears@gmail.com>
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryce Simonds <bsimonds14@gmail.com>

--- spec/facades/catalog_facade_spec.rb
+++ spec/facades/catalog_facade_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe CatalogFacade, :vcr do
     nearby_breweries = CatalogFacade.breweries('39.738453', '-104.984853', '50')
     expect(nearby_breweries.first).to be_an_instance_of(Brewery)
   end
+
+  it 'can return brewer beers from a poro with facade call', :vcr do
+    nearby_breweries = CatalogFacade.breweries('39.738453', '-104.984853', '50') 
+    brewer_beers = CatalogFacade.brewer_beers(nearby_breweries, "IPA")
+    expect(nearby_breweries.first).to be_an_instance_of(Brewery)
+    # expect(brewer_beers.first).to be_an_instance_of(Drink)
+  end
 end

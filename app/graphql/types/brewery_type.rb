d69da2bd1e6be607f713863e1790be2b699a4a03
Bryce Simonds <bsimonds14@gmail.com>
2022-10-19 19:29:09 -0600

    Added beers field with BeerType to form the relation between beers and breweries
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryan Shears <brshears@gmail.com>
    Co-authored-by: Thiago Silveira  <thiago1992@yahoo.com>

--- app/graphql/types/brewery_type.rb
+++ app/graphql/types/brewery_type.rb
@@ -4,13 +4,14 @@ module Types
   class BreweryType < Types::BaseObject
     field :name, String, null: true
     field :address, String, null: true 
-    field :distance_from_user, Integer, null: true 
+    field :distance_from_user, Float, null: true 
     field :website, String, null: true
     field :instagram, String, null: true
     field :facebook, String, null: true
     field :twitter, String, null: true
     field :brewery_description, String, null: true
     field :catalog_brewery_id, String, null: true 
+    field :beers, [BeerType], null: true
   end
 end
 

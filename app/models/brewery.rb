3bdbd0c23846d0da415acfb2cd2987e0be058463
Bryce Simonds <bsimonds14@gmail.com>
2022-10-19 19:24:52 -0600

    Changed distance from user data type from an integer to a float
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryan Shears <brshears@gmail.com>
    Co-authored-by: Thiago Silveira  <thiago1992@yahoo.com>

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,9 +5,9 @@ class Brewery < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :address
   validates_presence_of :distance_from_user
+  validates :brewery_description, presence: false 
   validates_presence_of :website
   validates_presence_of :instagram
   validates_presence_of :facebook
   validates_presence_of :twitter
-  # validates_presence_of :brewery_description, null:true
 end

02ca30eb5aed6ad10c1311b11ec74085b46b2f03
Bryce Simonds <bsimonds14@gmail.com>
2022-10-20 15:34:12 -0600

    Added validations for brewery model
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryan Shears <brshears@gmail.com>
    Co-authored-by: Thiago Silveira  <thiago1992@yahoo.com>

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,7 +5,7 @@ class Brewery < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :address
   validates_presence_of :distance_from_user
-  validates :brewery_description, presence: false 
+  validates_presence_of :brewery_description, allow_blank: true 
   validates_presence_of :website
   validates_presence_of :instagram
   validates_presence_of :facebook

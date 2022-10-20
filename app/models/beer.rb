7a5c63bd21ca67a38289bfc7c448914b5b0e411d
Bryce Simonds <bsimonds14@gmail.com>
2022-10-19 19:24:11 -0600

    Changed last modified data type from a string to an integer
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryan Shears <brshears@gmail.com>
    Co-authored-by: Thiago Silveira  <thiago1992@yahoo.com>

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,9 +4,9 @@ class Beer < ApplicationRecord
   validates_presence_of :catalog_beer_id
   validates_presence_of :name
   validates_presence_of :style
-  validates_presence_of :description
-  validates_presence_of :abv
-  validates_presence_of :ibu
+  validates :description, presence: false 
+  validates :ibu, presence: false 
+  validates :abv, presence: false 
   validates_presence_of :catalog_brewery_id
   validates_presence_of :last_modified
 end

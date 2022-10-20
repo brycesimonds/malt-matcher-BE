0a114d92be4bd9e17fe747f71ea150ee19a97510
Bryce Simonds <bsimonds14@gmail.com>
2022-10-19 19:26:08 -0600

    Refactored: edited to accept two arguments and initialized with appropriate and accurate data
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryan Shears <brshears@gmail.com>
    Co-authored-by: Thiago Silveira  <thiago1992@yahoo.com>

--- app/poros/drink.rb
+++ app/poros/drink.rb
@@ -5,16 +5,21 @@ class Drink
               :style,
               :description,
               :abv,
-              :ibu
+              :ibu,
+              :last_modified
 
   def initialize(data, data2)
-    @catalog_beer_id = data
-    @catalog_brewery_id = data
-    @name = data
-    @style = data
-    @description = data
-    @abv = data2
-    @ibu = data2
+    @catalog_beer_id = data[:id]
+    @catalog_brewery_id = data2[:brewer][:id]
+    @name = data[:name]
+    @style = data[:style]
+    @description = data2[:description]
+    @abv = data2[:abv]
+    @ibu = data2[:ibu]
+    @last_modified = format_time(data2[:brewer][:last_modified])
   end
 
+  def format_time(unix_time)
+    Time.at(unix_time).strftime("%Y-%m-%d").to_s
+  end
 end

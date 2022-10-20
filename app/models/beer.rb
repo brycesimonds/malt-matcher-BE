7064f1dc8105c2f2bede7503f16575ec2efecfe9
Bryce Simonds <bsimonds14@gmail.com>
2022-10-20 15:31:02 -0600

    Added validations for beer model
    Co-authored-by: Parker Thomson <pdthomson1239@gmail.com>
    Co-authored-by: Bryan Shears <brshears@gmail.com>
    Co-authored-by: Thiago Silveira  <thiago1992@yahoo.com>

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,9 +4,9 @@ class Beer < ApplicationRecord
   validates_presence_of :catalog_beer_id
   validates_presence_of :name
   validates_presence_of :style
-  validates :description, presence: false 
-  validates :ibu, presence: false 
-  validates :abv, presence: false 
+  validates_presence_of :description, allow_blank: true 
+  validates_presence_of :ibu, allow_blank: true 
+  validates_presence_of :abv, allow_blank: true 
   validates_presence_of :catalog_brewery_id
   validates_presence_of :last_modified
 end
